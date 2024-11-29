0cad25d35f4d60957d26ad3907d39650e6a84158
muhammadnur-alim <muhammad.alim@magic-jungle.co.id>
2024-11-29 17:06:49 +0700

    adding env

--- lib/grapqhl-cdk-stack.ts
+++ lib/grapqhl-cdk-stack.ts
@@ -31,8 +31,7 @@ export class GrapqhlCdkStack extends cdk.Stack {
       code: lambda.Code.fromAsset("lambda"),
       environment: {
         NODE_ENV: this.node.tryGetContext("env"),
-        MONGODB_URL:
-          "mongodb+srv://muhammadalim:@cluster0.57kx0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+        MONGODB_URL: process.env.MONGODB_URL!,
       },
     });
 
